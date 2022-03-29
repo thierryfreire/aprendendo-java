fc125b730efc05e7ec95da8652adba1af700ae49
Thierry Freire <thierryfreire37@gmail.com>
2022-03-28 23:51:53 -0300

    mensagem alterada

--- src/br/com/exemplo/Application.java
+++ src/br/com/exemplo/Application.java
@@ -2,6 +2,6 @@ package br.com.exemplo;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        System.out.println("Hello Git!!");
     }
 }
