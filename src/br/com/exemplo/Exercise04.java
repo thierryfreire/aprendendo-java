af656d8e0e4b6a7fcf34be4b29a23155ecb76cdb
Thierry Freire <thierryfreire37@gmail.com>
2022-04-05 11:53:59 -0300

    Exercicio 4 adicionado

---
+++ src/br/com/exemplo/Exercise04.java
@@ -0,0 +1,27 @@
+package br.com.exemplo;
+
+import java.util.Scanner;
+
+public class Exercise04 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite a nota 1: ");
+        double note1 = scanner.nextDouble();
+
+        System.out.println("Digite a nota 2: ");
+        double note2 = scanner.nextDouble();
+
+        System.out.println("Digite a nota 3: ");
+        double note3 = scanner.nextDouble();
+
+        System.out.println("Digite a nota 4: ");
+        double note4 = scanner.nextDouble();
+
+        double average =(note1 + note2 + note3 + note4) /2;
+
+        System.out.printf("Sua media e de: %s", average);
+
+    }
+}
