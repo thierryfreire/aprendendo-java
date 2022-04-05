b342239c0c7703657b96eecd473dc97d2a71482d
Thierry Freire <thierryfreire37@gmail.com>
2022-04-04 23:12:45 -0300

    Exercício 2 adicionado

---
+++ src/br/com/exemplo/Exercise02.java
@@ -0,0 +1,14 @@
+package br.com.exemplo;
+
+import java.util.Scanner;
+
+public class Exercise02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Me informe um número: ");
+        int number = scanner.nextInt();
+
+        System.out.printf("O número informado foi: %d", number);
+    }
+}
