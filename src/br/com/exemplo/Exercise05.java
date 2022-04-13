6b4806624839dbb96988b4d314c0255be4fc7ff3
Thierry Freire <thierryfreire37@gmail.com>
2022-04-12 23:13:01 -0300

    Exercicio 5 modificado

--- src/br/com/exemplo/Exercise05.java
+++ src/br/com/exemplo/Exercise05.java
@@ -2,15 +2,15 @@ package br.com.exemplo;
 
 import java.util.Scanner;
 
-public class Exercicio05 {
+public class Exercise05 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Informe valor em metros: ");
-        int metros = scanner.nextInt();
-        double resultado = metros * 100;
+        int meters = scanner.nextInt();
+        double result = meters * 100;
 
-        System.out.printf("Valor em metros e equivalete a %.2f cm.", resultado);
+        System.out.printf("Valor em metros e equivalete a %.2f cm.", result);
     }
 }
