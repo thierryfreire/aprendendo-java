933cbc5951a5f88660f08234a6c148767958e33a
Thierry Freire <thierryfreire37@gmail.com>
2022-04-19 00:44:01 -0300

    Exercicio 8 adicionado

--- src/br/com/exemplo/Exercise08.java
+++ src/br/com/exemplo/Exercise08.java
@@ -1,7 +1,21 @@
 package br.com.exemplo;
 
+import java.util.Scanner;
+
 public class Exercise08 {
     public static void main(String[] args) {
+
+        Scanner scanner =new Scanner(System.in);
+
+        System.out.print("Informe quanto ganha por hora:");
+        double hour = scanner.nextDouble();
+
+        System.out.print("Informe a horas trabalhada no mês:");
+        double workedHours = scanner.nextDouble();
+
+        double result = hour * workedHours;
+
+        System.out.printf("O salario referente ao mês é: %.2f", result);
 
     }
 }
