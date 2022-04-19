4a578713dbe58470a9ac23aac204d4a1f2d6e3c6
Thierry Freire <thierryfreire37@gmail.com>
2022-04-18 23:05:20 -0300

    Exercicio 7 adicionado

---
+++ src/br/com/exemplo/Exercise07.java
@@ -0,0 +1,17 @@
+package br.com.exemplo;
+
+import java.util.Scanner;
+
+public class Exercise07 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Informe o lado: ");
+        double side = scanner.nextDouble();
+
+        double squareArea = Math.pow(side, 2) ;
+
+        System.out.printf("O dobro da área do quadrado é: %.2f ", squareArea * 2);
+    }
+}
