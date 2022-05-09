b130b0c5c7c05b1fe91674fb32c00ee8eddbc525
Thierry Freire <thierryfreire37@gmail.com>
2022-05-09 11:16:48 -0300

    Exercicio 13 adicionado

---
+++ src/br/com/exemplo/Exercise13.java
@@ -0,0 +1,18 @@
+package br.com.exemplo;
+
+import java.util.Scanner;
+
+public class Exercise13 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite sua altura:");
+        double height = scanner.nextDouble();
+
+        double weightman = (72.7 * height) - 58;
+        double wieghtwoman = (62.1 * height) - 44.7;
+
+        System.out.printf("Peso ideal para homens: %.2f%n", weightman);
+        System.out.printf("Peso ideal para mulheres: %.2f", wieghtwoman);
+    }
+}
