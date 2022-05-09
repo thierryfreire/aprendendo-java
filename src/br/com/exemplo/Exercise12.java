cfee11816089ec8142eb3ef3a4a21bc4ed1382d1
Thierry Freire <thierryfreire37@gmail.com>
2022-05-09 11:02:21 -0300

    Exercicio 12 adicionado

---
+++ src/br/com/exemplo/Exercise12.java
@@ -0,0 +1,17 @@
+package br.com.exemplo;
+
+import java.util.Scanner;
+
+public class Exercise12 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite sua altura:");
+        double height = scanner.nextDouble();
+
+        double weight = (72.7 * height) - 58;
+
+        System.out.printf("Seu peso ideal é: %.2f kg",weight);
+    }
+}
