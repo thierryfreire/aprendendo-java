da9a4b6de153ac1a34abbca39e748f627267c3f0
Thierry Freire <thierryfreire37@gmail.com>
2022-05-24 10:06:33 -0300

    Execicio 14 adicionado

---
+++ src/br/com/exemplo/Exercise14.java
@@ -0,0 +1,22 @@
+package br.com.exemplo;
+
+import java.util.Scanner;
+
+public class Exercise14 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o peso total dos peixes:");
+        double fishWeight = scanner.nextDouble();
+
+        int limitWeight = 50;
+        double excess = fishWeight - limitWeight;
+        double finePerKilo = (fishWeight - limitWeight) * 4;
+
+        if (fishWeight > limitWeight) {
+            System.out.printf("Peso ultrapassou em %.2f kg e você terá que pagar multa de %.2f", excess, finePerKilo);
+        } else {
+            System.out.printf("O limite não foi ultrapassado");
+        }
+    }
+}
