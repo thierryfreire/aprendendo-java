e03be718ec16251f5a57c4afa32b61ecd448eb4b
Thierry Freire <thierryfreire37@gmail.com>
2022-05-27 08:47:44 -0300

    Exercicio 16 adicionado

--- src/br/com/exemplo/Exercise16.java
+++ src/br/com/exemplo/Exercise16.java
@@ -1,4 +1,28 @@
 package br.com.exemplo;
 
+import java.util.Scanner;
+
 public class Exercise16 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o tamanho da área a ser pintada:");
+        double areaSize = scanner.nextDouble();
+
+        int inkCover = 3;
+        int canCapacity = 18;
+        int canPrice = 80;
+
+        double liters = areaSize / inkCover;
+        double quantityCans = liters / canCapacity;
+
+        if (liters % canCapacity != 0) {
+            quantityCans += 1;
+        }
+        double amount = quantityCans * canPrice;
+
+        System.out.printf("Quantidade necessária de litros: %.2f%n", liters);
+        System.out.printf("Quantidade necessária de latas: %.2f%n", quantityCans);
+        System.out.printf("Valor total da compra:%.2f", amount);
+    }
 }
