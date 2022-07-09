389b630931f4c152bef340a295fe8b30f08c74bb
Thierry Freire <thierryfreire37@gmail.com>
2022-07-09 11:12:43 -0300

    Classe Quaddrado

---
+++ src/br/com/exemplo/classes/exercicio2/Main.java
@@ -0,0 +1,23 @@
+package br.com.exemplo.classes.exercicio2;
+
+public class Main {
+    public static void main(String[] args) {
+        Quadrado quadrado = new Quadrado(8);
+        Integer area = quadrado.getArea();
+        System.out.println(area);
+        byte s = 0;
+        short a = 1;
+        int b = 2;
+        long c = 3L;
+        float d = 4.5F;
+        double e = 8.2;
+        char f = 'A';
+        boolean g = false;
+        int[] numeros = {1,2,3,6,9,4};
+        System.out.println(numeros[4]);
+        Long k = 5L;
+        Double o = 6.3;
+        String w = "Thierry";
+
+    }
+}
---
+++ src/br/com/exemplo/classes/exercicio2/Quadrado.java
@@ -0,0 +1,14 @@
+package br.com.exemplo.classes.exercicio2;
+
+public class Quadrado {
+
+    private int side;
+
+    public Quadrado(int side){
+        this.side = side;
+    }
+
+    public int getArea() {
+        return side * side;
+    }
+}
