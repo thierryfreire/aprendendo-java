c13d1210cc438ebabad14cae9ab10662498f5748
Thierry Freire <thierryfreire37@gmail.com>
2022-07-14 22:10:17 -0300

    Exercicio 2

---
+++ src/br/com/exemplo/listas/exercicio2/Exerciciolista.java
@@ -0,0 +1,13 @@
+package br.com.exemplo.listas.exercicio2;
+
+public class Exerciciolista {
+    public static void main(String[] args) {
+        int[] numeros = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int i = numeros.length - 1; i >= 0; i--){
+            System.out.println(numeros[i]);
+
+        }
+    }
+
+}
