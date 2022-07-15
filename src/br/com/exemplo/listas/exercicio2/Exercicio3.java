7bcce17854f9ffe9411e8f96018b0a67b03c6917
Thierry Freire <thierryfreire37@gmail.com>
2022-07-15 09:33:42 -0300

    Exercicio 3

---
+++ src/br/com/exemplo/listas/exercicio2/Exercicio3.java
@@ -0,0 +1,32 @@
+package br.com.exemplo.listas.exercicio2;
+
+import java.util.Scanner;
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] notas = new int[4];
+
+        System.out.print("Digite a primeira nota:");
+        notas[0] = scanner.nextInt();
+
+        System.out.print("Digite a segunda nota:");
+        notas[1] = scanner.nextInt();
+
+        System.out.print("Digite a terceira nota:");
+        notas[2] = scanner.nextInt();
+
+        System.out.print("Digite a quarta nota:");
+        notas[3] = scanner.nextInt();
+
+        int soma = 0;
+        for (int nota : notas){
+            System.out.println(nota);
+            soma += nota;
+        }
+        double media = soma / 4.0;
+
+        System.out.print("A media é: " + media);
+    }
+}
